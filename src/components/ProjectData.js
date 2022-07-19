4392be44dea0e3b42e2a99809f417e983e3f511e
ahmedsmrt <ahmedsmrt@gmail.com>
2022-07-18 22:12:32 -0400

    Project Data Fill

--- src/components/ProjectData.js
+++ src/components/ProjectData.js
@@ -6,7 +6,7 @@ export const ProjectText = [
     title: "Sabr Natr",
     description:
       "As a way to motivate myself to travel to one of my favorite countries (Japan), I built a travel blog website with limited functionality to get a hang of the framework!",
-    languages: ["Javascript", "Html", "Css", "React"],
+    languages: ["Javascript", "Html", "Css", "React", "Figma" , "Photoshop"],
     screenshots: [
       "https://github.com/ahmedsmrt/Gatsby-Portfolio/blob/main/src/images/Sabr-large.jpg?raw=true",
       "https://raw.githubusercontent.com/ahmedsmrt/Gatsby-Portfolio/main/src/images/Image%201.jpg",
@@ -18,7 +18,7 @@ export const ProjectText = [
     id: 2,
     title: "Talk To Me Nice",
     description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec pharetra tellus ac lacus semper ullamcorper. Duis commodo bibendum eros, consectetur volutpat ex tincidunt vel. Nam augue est, fringilla in tempus sed, venenatis nec ante. Donec auctor ornare justo id convallis",
+      "This is a fun project where I got to explore of how to architect the backend with the frontend. It was a course inspired project that truly helps you put into scope what you need in order to create a full stack application. Check it out and Talk to me Nice!",
     languages: ["Javascript", "Html", "Css", "React", "Firebase"],
     screenshots: [
         "https://github.com/ahmedsmrt/Gatsby-Portfolio/blob/main/src/images/Image%203.jpg?raw=true",
@@ -31,20 +31,20 @@ export const ProjectText = [
     id: 3,
     title: "Personal Website Ver 2",
     description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec pharetra tellus ac lacus semper ullamcorper. Duis commodo bibendum eros, consectetur volutpat ex tincidunt vel. Nam augue est, fringilla in tempus sed, venenatis nec ante. Donec auctor ornare justo id convallis",
-    languages: ["Javascript", "Express", "Css", "Node"],
+      "This portfolio signified the start of my dive into dom traversal with Javascript and graphic design. Seeing just how creative I could get with what little coding experience I had at the time was a humbling yet exciting experience.",
+    languages: ["Javascript", "Html", "Css", "Figma"],
     screenshots: [
         "https://github.com/ahmedsmrt/Gatsby-Portfolio/blob/main/src/images/Image%206.jpg?raw=true",
         "https://github.com/ahmedsmrt/Gatsby-Portfolio/blob/main/src/images/Image%209.jpg?raw=true",
         "https://github.com/ahmedsmrt/Gatsby-Portfolio/blob/main/src/images/Image%2010.jpg?raw=true",
       ],
-      links: ['https://github.com/ahmedsmrt/Sabr-Natr', 'https://sabr-nature-blog.netlify.app/' ]
+      links: ['https://github.com/ahmedsmrt/Portfolio-Revamped', 'https://sabrdev.netlify.app/' ]
   },
   {
     id: 4,
     title: "Portfolio Moguls",
     description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec pharetra tellus ac lacus semper ullamcorper. Duis commodo bibendum eros, consectetur volutpat ex tincidunt vel. Nam augue est, fringilla in tempus sed, venenatis nec ante. Donec auctor ornare justo id convallis",
+      "A project from one of my courses that was tweaked to fit my personality. It's a Financial and Accounting website template that encourages a simple layout.",
     languages: ["Javascript", "Html", "Css", "Php"],
     screenshots: [
         "https://github.com/ahmedsmrt/Gatsby-Portfolio/blob/main/src/images/Image%2012.jpg?raw=true",
@@ -57,7 +57,7 @@ export const ProjectText = [
     id: 5,
     title: "First Portfolio Website",
     description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec pharetra tellus ac lacus semper ullamcorper. Duis commodo bibendum eros, consectetur volutpat ex tincidunt vel. Nam augue est, fringilla in tempus sed, venenatis nec ante. Donec auctor ornare justo id convallis",
+      "My first portfolio ever! After deciding to begin my journey to pursue a career in web development back in the end of 2018, this was the love child of three months worth of long nights in tutorial hell.",
     languages: ["Javascript", "Html", "Css", "Php"],
     screenshots: [
         "https://github.com/ahmedsmrt/Gatsby-Portfolio/blob/main/src/images/Image%207.jpg?raw=true",
