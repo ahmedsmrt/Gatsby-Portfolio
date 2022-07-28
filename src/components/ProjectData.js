f3e23d8c84f36d3ab6fb4f19aeef8cc8d29799a1
ahmedsmrt <ahmedsmrt@gmail.com>
2022-07-28 06:44:27 -0400

    Project Addition

--- src/components/ProjectData.js
+++ src/components/ProjectData.js
@@ -3,6 +3,19 @@ import React from "react";
 export const ProjectText = [
   {
     id: 1,
+    title: "Google Stylin",
+    description:
+      "As a way to motivate myself to travel to one of my favorite countries (Japan), I built a travel blog website with limited functionality to get a hang of the framework!",
+    languages: ["Javascript", "Html", "Css", "React"],
+    screenshots: [
+      "https://github.com/ahmedsmrt/Gatsby-Portfolio/blob/main/src/images/Image%2017.jpg?raw=true",
+      "https://github.com/ahmedsmrt/Gatsby-Portfolio/blob/main/src/images/Image%2016.jpg?raw=true",
+      "https://github.com/ahmedsmrt/Gatsby-Portfolio/blob/main/src/images/Image%2018.jpg?raw=true",
+    ],
+    links: ['https://github.com/ahmedsmrt/Google-Font-Comparison-App', 'https://sabr-nature-blog.netlify.app/' ]
+  },
+  {
+    id: 2,
     title: "Sabr Natr",
     description:
       "As a way to motivate myself to travel to one of my favorite countries (Japan), I built a travel blog website with limited functionality to get a hang of the framework!",
@@ -15,7 +28,7 @@ export const ProjectText = [
     links: ['https://github.com/ahmedsmrt/Sabr-Natr', 'https://sabr-nature-blog.netlify.app/' ]
   },
   {
-    id: 2,
+    id: 3,
     title: "Talk To Me Nice",
     description:
       "This is a fun project where I got to explore how to architect the backend with the frontend. It was a course inspired project that truly helps you put into scope what you need in order to create a full stack application. Check it out and Talk to me Nice!",
@@ -28,7 +41,7 @@ export const ProjectText = [
       links: ['https://github.com/ahmedsmrt/chat-too-much', 'https://chat-too-much-11vudry8q-ahmedsmrt.vercel.app/login' ]
   },
   {
-    id: 3,
+    id: 4,
     title: "Personal Website Ver 2",
     description:
       "This portfolio signified the start of my dive into dom traversal with Javascript and graphic design. Seeing just how creative I could get with what little coding experience I had at the time was a humbling yet exciting experience.",
@@ -41,7 +54,7 @@ export const ProjectText = [
       links: ['https://github.com/ahmedsmrt/Portfolio-Revamped', 'https://sabrdev.netlify.app/' ]
   },
   {
-    id: 4,
+    id: 5,
     title: "Portfolio Moguls",
     description:
       "A project from one of my courses that was tweaked to fit my personality. It's a Financial and Accounting website template that encourages a simple layout.",
@@ -54,7 +67,7 @@ export const ProjectText = [
       links: ['https://github.com/ahmedsmrt/Portfolio-Moguls.github.io', 'https://ahmedsmrt.github.io/Portfolio-Moguls.github.io/' ]
   },
   {
-    id: 5,
+    id: 6,
     title: "First Portfolio Website",
     description:
       "My first portfolio ever! After deciding to begin my journey to pursue a career in web development back in the end of 2018, this was the love child of three months worth of long nights in tutorial hell.",
