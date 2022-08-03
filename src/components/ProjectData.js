4b08958163ef92f455a31111325043421c2d77e2
ahmedsmrt <ahmedsmrt@gmail.com>
2022-08-03 18:27:47 -0400

    Project

--- src/components/ProjectData.js
+++ src/components/ProjectData.js
@@ -29,6 +29,19 @@ export const ProjectText = [
   },
   {
     id: 3,
+    title: "Notes With Nuance Sign Up",
+    description:
+      "This is a part of a larger personal project that I am building to help encourage journaling for mental health purposes. This mini project helps demonstrate handling form data and React hook and side effects management!",
+    languages: ["Javascript", "Html", "Css", "React", "Figma" , "Adobe XD"],
+    screenshots: [
+      "https://github.com/ahmedsmrt/Gatsby-Portfolio/blob/main/src/images/Sabr-large.jpg?raw=true",
+      "https://raw.githubusercontent.com/ahmedsmrt/Gatsby-Portfolio/main/src/images/Image%201.jpg",
+      "https://github.com/ahmedsmrt/Gatsby-Portfolio/blob/main/src/images/Image%202.jpg?raw=true",
+    ],
+    links: ['https://github.com/ahmedsmrt/Sabr-Natr', 'https://sabr-nature-blog.netlify.app/' ]
+  },
+  {
+    id: 4,
     title: "Talk To Me Nice",
     description:
       "This is a fun project where I got to explore how to architect the backend and connect it to the frontend. It was a course project that truly helped me put into scope what I needed in order to create a full stack application from a high level perspective. Check it out and Talk to me Nice!",
@@ -41,7 +54,7 @@ export const ProjectText = [
       links: ['https://github.com/ahmedsmrt/chat-too-much', 'https://chat-too-much-11vudry8q-ahmedsmrt.vercel.app/login' ]
   },
   {
-    id: 4,
+    id: 5,
     title: "Personal Website Ver 2",
     description:
       "This portfolio signified the start of my dive into dom traversal with Javascript and graphic design. Seeing just how creative I could get with what little coding experience I had at the time was a humbling yet exciting experience.",
@@ -54,7 +67,7 @@ export const ProjectText = [
       links: ['https://github.com/ahmedsmrt/Portfolio-Revamped', 'https://sabrdev.netlify.app/' ]
   },
   {
-    id: 5,
+    id: 6,
     title: "Portfolio Moguls",
     description:
       "A project from one of my courses that was tweaked to fit my personality. It's a Financial and Accounting website template that encourages a simple layout.",
@@ -67,7 +80,7 @@ export const ProjectText = [
       links: ['https://github.com/ahmedsmrt/Portfolio-Moguls.github.io', 'https://ahmedsmrt.github.io/Portfolio-Moguls.github.io/' ]
   },
   {
-    id: 6,
+    id: 7,
     title: "First Portfolio Website",
     description:
       "My first portfolio ever! After deciding to begin my journey to pursue a career in web development back in the end of 2018, this was the love child of three months worth of endless nights in tutorial hell.",
